221fcb362650376986218b813b3a0b83640fa45b
Zach Wagner <zach.wagner@spothero.com>
2019-01-25 20:02:54 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+    div.container
+        h1 {{ message }}
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            message: 'Zachary Wagner - Parcel + Vue',
+        };
+    },
+};
+</script>
+
+<style scoped>
+    .container {
+        width: 600px;
+        margin: 50px auto;
+        text-align: center;
+    }
+</style>
